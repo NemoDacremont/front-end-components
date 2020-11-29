6100f87a2f0f99079e7d42e32098c68392711176
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-11-29 10:33:04 +0000

    Created app.vue html outline

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<header>
+		<nav id="nav">
+		</nav>
+	</header>
+  
+	<main>
+		<router-view/>
+	</main>
+
+	<footer>
+		<p>Nemo D'acremont | <a href="https://github.com/odasta/">https://github.com/odasta</a></p>
+	</footer>
 </template>
 
 <style lang="scss">
